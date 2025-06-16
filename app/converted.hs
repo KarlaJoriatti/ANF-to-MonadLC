acaa96530bfa77424cc00fca6f41df7dc4bea299
KarlaJoriatti <kjoriatti@gmail.com>
2025-06-16 12:15:10 -0300

    factorial test fixed

--- app/converted.hs
+++ app/converted.hs
@@ -9,29 +9,22 @@ data Console x where
 
 print x = send (Print x)
 
-data Exception x where
-   Throw :: P.String -> Exception () 
-
-throw x0 = send (Throw x0)
-
 eapp :: P.Monad m => m (a -> m b) -> m a -> m b
 eapp f x = do
    res <- f P.<*> x
    res
 
 
-safedivision :: (Member Exception t1) => P.Int -> Eff t0 (P.Int -> Eff t1 P.Int )
-safedivision a0 = 
-  P.return P.$ \b0 -> do
-   let bl2 _ = do 
-           let bl3 _ = do 
-                   _ <- ( P.return throw `eapp` P.return "\"can't divide by zero\"")
-                   ( P.return bl4 `eapp` P.return ())
-               bl4 _ = do 
-                   P.return (a0  `P.div`  b0)
-           f0 <- P.return (b0  P.==  0)
-           if f0
-           then ( P.return bl3 `eapp` P.return ())
-           else ( P.return bl4 `eapp` P.return ())
-   ( P.return bl2 `eapp` P.return ())
+fatorial :: P.Int -> Eff t0 P.Int 
+fatorial n0 = do
+  let bl2 _ = do 
+          let bl3 _ = do 
+                  ( ( P.* )  P.<$> P.return n0 P.<*> ( P.return fatorial `eapp` P.return (n0  P.-  1)))
+              bl4 _ = do 
+                  P.return 1
+          f0 <- P.return (n0  P.>  1)
+          if f0
+          then ( P.return bl3 `eapp` P.return ())
+          else ( P.return bl4 `eapp` P.return ())
+  ( P.return bl2 `eapp` P.return ())
 
